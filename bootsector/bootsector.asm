bb9fe547491b0248d16de392b5ccdc2bcf7e8fc5
phulsechinmay <phulsechinmay@gmail.com>
2020-05-09 02:03:33 -0500

    Hello world bootsector

--- bootsector/bootsector.asm
+++ bootsector/bootsector.asm
@@ -1,3 +1,27 @@
+; Make BIOS output 'Hello world'
+mov ah, 0x0e ; Tele-type output mode for BIOS interrupt
+mov al, 'H'
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+int 0x10
+mov al, 'o'
+int 0x10
+mov al, ' '
+int 0x10
+mov al, 'W'
+int 0x10
+mov al, 'o'
+int 0x10
+mov al, 'r'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'd'
+int 0x10
+
 jmp $ ; Infinite loop
 
 ; Padding with 0s until magic number
